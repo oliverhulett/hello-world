911d6f43f67effd8e20d45f9afc82e65b5855319
Oliver Hulett <ohulett@atlassian.com>
2019-09-19 08:49:38 +1000

    Need to fix test, got media types working

--- src/main/java/com/cap/backend/rest/ProductResource.java
+++ src/main/java/com/cap/backend/rest/ProductResource.java
@@ -3,6 +3,7 @@ package com.cap.backend.rest;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/test/java/com/cap/backend/ApplicationTests.java
+++ src/test/java/com/cap/backend/ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.cap.backend;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,9 +8,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class SearsApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.fail("Test does not fail, even if DB doesn't exist or has wrong schema");
 	}
 }

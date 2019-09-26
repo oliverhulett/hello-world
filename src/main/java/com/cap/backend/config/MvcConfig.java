cef85496462b1862448fb267e764f439cc7323e6
Oliver Hulett <ohulett@atlassian.com>
2019-09-26 22:40:32 +1000

    Some config, didn't work?

--- src/main/java/com/cap/backend/config/MvcConfig.java
+++ src/main/java/com/cap/backend/config/MvcConfig.java
@@ -1,11 +1,13 @@
 package com.cap.backend.config;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
+@EnableAutoConfiguration
 @ComponentScan({"com.cap.backend"})
 public class MvcConfig {
 	//

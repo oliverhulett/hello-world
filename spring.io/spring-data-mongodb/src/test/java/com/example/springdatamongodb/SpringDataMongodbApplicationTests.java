dca88f5083683855957ecab476709561d9b830e9
Oliver Hulett <ohulett@atlassian.com>
2019-11-16 23:15:36 +1100

    MongoDB works

---
+++ spring.io/spring-data-mongodb/src/test/java/com/example/springdatamongodb/SpringDataMongodbApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.springdatamongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
